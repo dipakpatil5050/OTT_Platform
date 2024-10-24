4542a0ad5537a2a4e1a8dc4e280c6700f7c2828f
Dipak Patil <dipakup.a04@gmail.com>
2024-10-24 23:26:04 +0530

    Feat : All Setup configuration for backend project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-// require("dotenv").config({ path: "./env" });
-
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
@@ -8,14 +6,15 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB().then(() => {
-  app.listen(process.env.PORT || 8000. ()=> {
-    console.log(`Server is Running on Port ${process.env.PORT}`);
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is Running on Port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo Db Connection Failed: " + err);
   });
-})
-.catch((err) => {
-  console.log("Mongo Db Connection Failed: " + err)
-});
 
 // import express from "express";
 // const app = express();
